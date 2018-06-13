1b93fff4639e729f559889b64c1254a735cce169
diveki <divekizs@yahoo.com>
2018-06-13 18:01:17 +0100

    Added tkinter key press feature. However it is not combined yet with the game. Reworked the classes too.

--- classes.py
+++ classes.py
@@ -7,6 +7,7 @@ Created on Tue Jun 12 13:04:33 2018
 
 from random import randint
 from time import sleep, time
+from tkinter import *
 
 class Player:
     def __init__(self, name):
@@ -20,59 +21,77 @@ class Player:
     
     def get_button(self):
         return self.button
+    
+    def set_button(self, button):
+        self.button = button
         
 
 class GeneralButton:
     def __init__(self, cls):
         self.initializeButton(cls)
 
+
 class KeyboardButton(GeneralButton):
     def initializeButton(self, cls):
         button = input('Press the button you want to use: ')
         cls.button = button
-    
-    
 
-class Game:
-    def __init__(self, numberPlayer, buttonType, roundNumbers=3, msg = 'Press'):
+
+class GeneralGame(Frame):
+    def __init__(self, master, numberPlayer=2, roundNumbers=3, **kw):
+        Frame.__init__(self, master)
         self.numberOfPlayers = numberPlayer
-        self.buttonClass = buttonType
         self.numberOfRounds = roundNumbers
-        self.startmsg = msg
         self.maxWait = 5
         self.players = {}
+        self.__dict__.update(kw)
         self.initializeGame()
-        
+    
     def initializeGame(self):
         for ii in range(self.numberOfPlayers):
             self.players[ii+1] = self.set_players(ii+1)
-            #self.set_buttons()
     
-    def set_players(self, pid):
-        name = input('Name of %sth player: ' % pid)
-        p = Player(name)
-        self.buttonClass(p)
-        return p
-        
     def startGame(self):
         for ii in range(self.numberOfRounds):
             self.startMessage(ii+1)
             startTime = time()
             self.startSignal()
             
+        
+class ButtonGame(GeneralGame):
+    def __init__(self, master, numberPlayer=2, roundNumbers=3, msg = 'Press', **kw):
+        GeneralGame.__init__(self, master, numberPlayer=numberPlayer, roundNumbers=roundNumbers, **kw)
+        self.startmsg = msg
+        self.master = master
+        
+        
+    def set_players(self, pid):
+        name = input('Name of %sth player: ' % pid)
+        p = Player(name)
+        button = input('Press the button you want to use: ')
+        p.set_button(button)
+        return p
+    
     def startMessage(self, ii):
         print('Round number %s starts when %s appears' % (ii, self.startmsg))
-    
+
     def startSignal(self):
         sec = randint(1, self.maxWait)
         sleep(sec)
         print(self.startmsg)
         
     
+def pressedButton(event):
+    print("pressed", repr(event.char))
+    
+
     
 if __name__ == '__main__':
-    game = Game(2, KeyboardButton, roundNumbers=3)
-    game.startGame()
+    root = Tk()
+    game = ButtonGame(root, msg = 'Press')
+    root.bind("<Key>", pressedButton)
+  #  game.startGame()
+    root.mainloop()
     
     
     
